1542fb43685cceb3f452755181315f27aa73ee6e
Alisson Lima <amlxd5@gmail.com>
2022-06-23 18:25:02 -0500

    Iniciando 2º dia Ignite lab

---
+++ lab/event-plataform/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "./Logo";
+
+export function Header() {
+    return (
+        <header className="w-full py-5 items-center justify-content" >
+            <Logo />
+        </header>
+        )
+}
