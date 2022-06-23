d0bc2f979fa4701bc8dceddcf8997278b5b2c8e4
Alisson Lima <amlxd5@gmail.com>
2022-06-23 13:15:30 -0500

    Ignite Lab - Day 1

---
+++ lab/event-plataform/src/App.tsx
@@ -0,0 +1,43 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LESSONS_QUERY = gql `
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
+function App() {
+
+  /* Método sem o useQuery*/
+
+  // useEffect(() => {
+  //   client.query({
+  //     query: GET_LESSONS_QUERY,
+  //   }).then(response => {
+  //     console.log(response.data);
+  //   })
+  // }, [])
+
+  /* Método usando useQuery*/
+
+  const {data} = useQuery<{ lessons: Lesson[]}> (GET_LESSONS_QUERY)
+
+  
+  return (
+      <ul>
+        {data?.lessons.map(lesson => {
+          return <li key={lesson.id}>{lesson.title}</li>
+        })}
+      </ul>
+    )
+}
+
+export default App
