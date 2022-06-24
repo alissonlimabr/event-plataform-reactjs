690d201bb72360e3368f3e534e08d8782b30b3df
Alisson Lima <amlxd5@gmail.com>
2022-06-24 01:23:20 -0500

    Ignite Lab - Day 2 concluído

--- lab/event-plataform/src/components/Header.tsx
+++ lab/event-plataform/src/components/Header.tsx
@@ -2,7 +2,13 @@ import { Logo } from "./Logo";
 
 export function Header() {
     return (
-        <header className="w-full py-5 items-center justify-content" >
+        // Cores personalizadas definidas no tailwind.config.js
+        //  border-b = borda de 1 px
+        //  w-full = width full
+        //  flex = display flex
+        // py-5 = padding vertical de 20px. Para calcular em px tem que multiplicar por 4.
+        
+        <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600" >
             <Logo />
         </header>
         )
--- lab/event-plataform/src/components/Lesson.tsx
+++ lab/event-plataform/src/components/Lesson.tsx
@@ -1,5 +1,58 @@
-export function Lesson() {
+// rounded = bordas arredondadas (padrão 4px)
+// justify-between = jorga o conteúdo para as extremidades
+// text-sm = define font size e altura da linha (tamanho 14)
+// text-xs = menor que o sm (tamanho 12)
+// px = padding no eixo X, ou seja, horizontal
+// py-[1.125rem] = padding no eixo Y, ou seja, vertical. Aprox. 2px
+// border = adiciona uma borda
+// gap = espaçamento entre o conteúdo
+// Para ajustar o ícone =  flex, items-center e gap-2
+// {props.title} = variável javascript sendo exibida dentro do html (necessário uso de chaves)
+import { isPast, format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { CheckCircle, Lock } from 'phosphor-react';
+
+// Define as propriedades do component desse component
+interface LessonProps {
+    title: string;
+    slug: string;
+    avaliableAT: Date;
+    type: 'live' | 'class'
+}
+
+// exporta a função e recebe as propriedades de LessonProps
+export function Lesson(props: LessonProps) {
+    const isLessonAvailable = isPast(props.avaliableAT);
+    const isLessonAvailableDateFormat = format(props.avaliableAT, "EEEE ' • ' d ' de ' MMMM ' • ' k'h'mm", {
+        locale: ptBR,
+    });
+
     return (
-        <h1>Lesson</h1>
+    <a href="#">
+        <span className="text-gray-300">
+        {isLessonAvailableDateFormat}
+        </span>
+        <div className="rounded border border-gray-500  p-4 mt-2">
+            <header className="flex items-center justify-between">
+                {isLessonAvailable ? (
+                    <span className="text-sm text-blue-500 flex items-center gap-2">
+                    <CheckCircle size={20}/>
+                    Conteúdo Liberado
+                    </span>
+                ) : (
+                    <span className="text-sm text-orange-500 flex items-center gap-2">
+                    <Lock size={20}/>
+                    Em Breve
+                </span>
+                )}
+                <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
+                {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+                </span>
+            </header>
+            <strong className="text-gray-200 mt-5 block">
+            {props.title}
+            </strong>
+        </div>
+    </a>   
     )
 }
