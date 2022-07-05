20a37215092df667b02a30a616054f0c767e8586
Alisson Lima <amlxd5@gmail.com>
2022-07-05 01:29:56 -0500

    Melhorias no footer

--- lab/event-plataform/src/components/Footer.tsx
+++ lab/event-plataform/src/components/Footer.tsx
@@ -2,13 +2,13 @@ import LogoFooter from "./LogoFooter";
 
 export function Footer() {
   return (
-    <footer className="bg-gray-900 flex items-center">
-        <div className="px-6"> 
+    <footer className="flex items-center border-t-2 border-t-gray-600 mx-6 ">
+        <div className="mr-6"> 
           <LogoFooter />
         </div>
         <div className="w-full my-6 flex justify-between ">
-          <p className="px-6 text-gray-30 text-[16px]"> Rocketseat - Todos os direitos reservados</p>
-          <p className="px-6 text-gray-300 text-[16px]">Política de privacidade</p>
+          <p className="text-gray-300 text-[16px]"> Rocketseat - Todos os direitos reservados</p>
+          <p className="text-gray-300 text-[16px]">Políticas de privacidade</p>
         </div>
     </footer>
   );
