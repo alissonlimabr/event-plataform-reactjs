7da2afa7c4144c975fc2f968ae0232c254ca1854
Alisson Lima <amlxd5@gmail.com>
2022-07-17 16:59:12 -0500

    Adicionado ícone React em Subscribe

--- lab/event-plataform/src/pages/Subscribe.tsx
+++ lab/event-plataform/src/pages/Subscribe.tsx
@@ -16,11 +16,11 @@
 // {loadind} = true ou false. Usado para verificar se a mutation ainda está sendo feita(true) ou se já terminou(False)
 // disabled={loading} = desabilitado enquanto o loading for true
 import { gql, useMutation } from "@apollo/client";
-import { useState, FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Footer } from "../components/Footer";
 import { Logo } from "../components/Logo";
-import imgMockup from "/src/assets/code-mockup.png"
+import { Mockup } from "../components/Mockup";
 
 // conexão com graphCMS
 const CREATE_SUBSCRIBER_MUTATION = gql `
@@ -61,55 +61,61 @@ export function Subscribe() {
     return (
     <div>
         <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-            {/* Div que contém a descrição e o formulário de inscrição */}
-            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-                {/* Descrição */}
-                <div className="max-w-[640px]">
-                    <Logo />
-                    <h1 className="mt-8 text-[2.5rem] leading-tight">
-                    Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong> 
-                    </h1>
+            <div className="bg-icon min-h-[575px] min-w-[654px] bg-top mt-[10px] bg-no-repeat">
+                {/* Div que contém a descrição e o formulário de inscrição */}
+                <div className="w-full max-w-[1100px]  flex items-stretch justify-between mt-20 mx-auto">
+                    {/* Descrição */}
+                    <div className="max-w-[640px]">
+                        <Logo />
+                        <h1 className="mt-8 text-[2.5rem] leading-tight">
+                        Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong> 
+                        </h1>
 
-                    <p className="mt-4 text-gray-200 leading-relaxed">
-                    Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
-                    </p>
+                        <p className="mt-4 text-gray-200 leading-relaxed">
+                        Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
+                        </p>
+                    </div>
+
+                    {/* Formulário de inscrição */}
+                    <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+                        <strong className="text-2xl mb-6 block ">
+                            Inscreva-se gratuitamente
+                        </strong>
+                        <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
+                            <input 
+                            className="bg-gray-900 rounded px-5 h-14"
+                            type="text" 
+                            placeholder="Seu nome completo" 
+                            onChange={event => setName(event.target.value)}
+                            />
+
+                            <input 
+                            className="bg-gray-900 rounded px-5 h-14"
+                            type="email" 
+                            placeholder="Digite o seu e-mail"
+                            onChange={event => setEmail(event.target.value)}
+                            />
+
+                            <button
+                            className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-small hover:bg-green-700 transition-colors disabled:opacity-50" 
+                            type="submit"
+                            disabled={loading}
+                            >
+                                Garantir minha vaga
+                            </button>
+                        </form>
+                    </div>
                 </div>
-                {/* Formulário de inscrição */}
-                <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-                    <strong className="text-2xl mb-6 block ">
-                        Inscreva-se gratuitamente
-                    </strong>
-                    <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
-                        <input 
-                        className="bg-gray-900 rounded px-5 h-14"
-                        type="text" 
-                        placeholder="Seu nome completo" 
-                        onChange={event => setName(event.target.value)}
-                        />
-
-                        <input 
-                        className="bg-gray-900 rounded px-5 h-14"
-                        type="email" 
-                        placeholder="Digite o seu e-mail"
-                        onChange={event => setEmail(event.target.value)}
-                        />
-
-                        <button
-                        className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-small hover:bg-green-700 transition-colors disabled:opacity-50" 
-                        type="submit"
-                        disabled={loading}
-                        >
-                            Garantir minha vaga
-                        </button>
-
-                    </form>
+                <div>
+                    <Mockup />
                 </div>
             </div>
-            <img src= {imgMockup} className="mt-10" alt="" />
         </div>
+        
         <div>
         <Footer />
         </div>
+
     </div>
         
     )
