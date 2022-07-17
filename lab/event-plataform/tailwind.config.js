8b5b09cec9cfbe923753da22e9b6ef0f4370d7bb
Alisson Lima <amlxd5@gmail.com>
2022-07-17 16:59:25 -0500

    Importação do ícone React

--- lab/event-plataform/tailwind.config.js
+++ lab/event-plataform/tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        blur: 'url(/src/assets/blur-background.png)'
+        blur: 'url(/src/assets/blur-background.png)',
+        icon: 'url(/src/assets/Reactjs-icon-background.png)',
       },
       fontFamily: {
         sans: 'Roboto, sans-serif'
